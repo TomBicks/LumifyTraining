0fdd8399159bd5e92e10233f500bc63567415f4f
Thomas Bickley <49082698+TomBicks@users.noreply.github.com>
2025-11-24 23:04:15 +1100

    Dummy JS function

---
+++ Front-End Work/JSTest.js
@@ -0,0 +1,7 @@
+//Enabling Strict Mode is better for determining errors and writing more secure code
+
+function createNavButton (name) {
+	this.name = name;
+}
+
+var homeButton = new createNavButton("Home");
