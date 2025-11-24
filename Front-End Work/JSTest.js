bbe82d13972681e4763b8bcc6d044f3b388e1736
Thomas Bickley <49082698+TomBicks@users.noreply.github.com>
2025-11-24 23:06:52 +1100

    Prototyping, including prototyping functions; very useful!

--- Front-End Work/JSTest.js
+++ Front-End Work/JSTest.js
@@ -1,7 +1,10 @@
 //Enabling Strict Mode is better for determining errors and writing more secure code
 
-function createNavButton (name) {
+function navButton (name) {
 	this.name = name;
+	navButton.prototype.getNoOfNavButtons: function() {
+		console.log("This is a prototyped function, meaning every object of navButton all have pointers to a single function, rather than each having their own copy of the same function. Good if the function would do the same thing for every single instance.")
+	}
 }
 
-var homeButton = new createNavButton("Home");
+var homeButton = new navButton("Home");
