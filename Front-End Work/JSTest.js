442b527f8b8a7a96d8fe454bc2ca170367a64b7a
Thomas Bickley <49082698+TomBicks@users.noreply.github.com>
2025-11-25 17:20:00 +1100

    Added JS file source, and finally tested and ran the function of a prototype

--- Front-End Work/JSTest.js
+++ Front-End Work/JSTest.js
@@ -1,19 +1,23 @@
 //Enabling Strict Mode is better for determining errors and writing more secure code
+console.log("JavaScript File has Loaded");
 
 function navButton(name) {
 	this.name = name;
-	}
-}
+};
 
 function exitButton(name) {
 	this.name = name;
-}
+};
 
 function Button(type) {
-	this.type = type
-	this.getNoOfButtonsOfType: function() {
+	this.type = type;
+	this.getNoOfButtonsOfType = function() {
 		console.log("This is an example of an inherited function, via prototyping. By having tthis Button object constructor, we can create a prototype of both nav and exit buttons, thus meaning any nav and exit buttons created will have a reference to this Button prototype, and therefore have a reference to the 'getNoOfButtonsOfType' function");
-	}
-}
+	};
+};
+
+navButton.prototype = new Button("navButton");
+exitButton.prototype = new Button("exitButton");
 
 var homeButton = new navButton("Home");
+homeButton.getNoOfButtonsOfType();
