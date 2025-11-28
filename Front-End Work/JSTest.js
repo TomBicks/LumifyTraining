9b63e6f98d80556031a7da6488c76424941cf3f0
Thomas Bickley <49082698+TomBicks@users.noreply.github.com>
2025-11-28 13:32:47 +1100

    Important string manipulation functions

--- Front-End Work/JSTest.js
+++ Front-End Work/JSTest.js
@@ -75,12 +75,52 @@ var name_str2 = "David";
 var name_obj1 = new String("David");
 var name_obj2 = new String("David");
 
+//Comparisons
 console.log("Is name_str1 == name_str2? ", name_str1 == name_str2);
 console.log("Is name_str1 == name_obj1? ", name_str1 == name_obj1);
 console.log("Is name_obj1 == name_obj2? ", name_obj1 == name_obj2);
 console.log("Is the valeuOf name_obj1 double equal to valeuOf name_obj2? : ", name_obj1.valueOf() == name_obj2.valueOf());
 
+//Strict Comparisons
 console.log("Is name_str1 === name_str2? ", name_str1 === name_str2);
 console.log("Is name_str1 === name_obj1? ", name_str1 === name_obj1);
 console.log("Is name_obj1 === name_obj2? ", name_obj1 === name_obj2);
 console.log("Is the valeuOf name_obj1 triple equal to valeuOf name_obj2? : ", name_obj1.valueOf() === name_obj2.valueOf());
+
+
+//Useful string functions
+let empName = "Jane Chang";
+console.log("Number of characters in empName is : " + empName.length);
+console.log("Index of h is : "+ empName.indexOf("h"));
+console.log("'Ch' occurs at position : " + empName. search("Ch"));
+
+//String slices
+var line = "Fundamentals of JavaScript";
+console.log("Original line: ", line);
+console.log("Slice (1, 12): ", line.slice(1,12));
+console.log("Slice (-23, -15): ", line.slice(-23, -15));
+console.log("Slice (13): ", line.slice(13));
+
+//Substitute substrings into string using replace
+console.log("Replacing 'JavaScript' with 'Web Programming'",
+line.replace("JavaScript", "Web Programming") );
+
+console.log("Replacing 'javascript' with 'Python'", //Replace is case-sensitive, so this won't replace anything
+line.replace("javascript", "Python"));
+
+console.log("Replacing 'javascript/i' with 'Python'", //"/i" makes the substituion case-insensitive
+line.replace(/javascript/i, "Python"));
+
+console.log("Default replace of 'a' with 'A': ", //Replace only replaces the first instance of the desired substring
+line.replace("a", "A") );
+
+console.log("Global replace of 'a' with 'A': ", //"/g" allows the replacement of all instances of the desired substring
+line.replace(/a/g, "A"));
+
+//Splitting a string
+console.log("Splitting the line on space: ", line.split(" "));
+
+//Trimming a string
+var myString = "                Hey, this is JS                      "; //Doesn't trim any spaces between the words however
+console.log("Before the trim operation : " + myString);
+console.log("After the trim operation : " + myString.trim());
