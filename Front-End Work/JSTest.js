ed24df9babda5ee272342912bf57f2a1eb3979ee
Thomas Bickley <49082698+TomBicks@users.noreply.github.com>
2025-12-04 00:06:47 +1100

    Destructuring arrays via spreading
    By spreading arrays, each index's item of the array can be assigned to a different variable, thereby destructuring it.
    By matching an array to an array of variables, and with the use of spreading, the indexes pulled can be manipulated.

--- Front-End Work/JSTest.js
+++ Front-End Work/JSTest.js
@@ -296,3 +296,33 @@ console.log("My name (spread): ", ...myName);
 
 //We can also create arrays from, for example, spread strings, using Array.from()
 console.log("Array formed with Array.from(myName):", Array.from(myName));
+
+
+//Destructuring arrays via spreading
+let numbers = [10, 20, 30, 40];
+
+//Matches the values to each index
+let [a, b, c, d] = numbers;
+console. log("Values after the first assignment (no rest parameters) : \n")
+console. log("Value of a : " + a);
+console. log("Value of b : " + b);
+console. log("Value of c : " + c) ;
+console. log("Value of d : " + d);
+
+//Grabs the first three values
+[a, b, c] = numbers;
+console. log("Values after the second assigment (no rest parameters) : \n")
+console. log("Value of a : " + a);
+console. log("Value of b : " + b);
+console. log("Value of c : " + c);
+
+//a grabs the first value, and the rest are grabbed by b
+[a, ...b] = numbers;
+console. log("Values after [a, ...b] = numbers:\n")
+console. log("Value of a : " + a);
+console. log("Value of b : " + b);
+
+//Ignores the first value, and the rest are grabbed by a/g
+[,...a] = numbers;
+console. log("Values after [, ...a] = numbers:\n")
+console. log("Value of a : " + a);
