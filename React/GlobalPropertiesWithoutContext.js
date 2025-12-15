d2d8843f8d82566e8469964f9ffcd581f405aa6e
Thomas Bickley <49082698+TomBicks@users.noreply.github.com>
2025-12-16 00:28:25 +1100

    Global properties with no context
    Not sure how helpful this is; think it's mainly showing how having to add theme="orange" to every single little element gets tedious *really* quickly, and offers a solution next example

---
+++ React/GlobalPropertiesWithoutContext.js
@@ -0,0 +1,37 @@
+class ThemedBorderBackground extends React.Component {
+	render() {
+		return(
+			<div className = {'bt bt-' + this.props.theme + ' bg-' + this.props.theme}>{this.props.children}</div>
+		);
+	}
+}
+
+class ThemedButton extends React.Component {
+	render() {
+		return(
+			<div>
+				<button className = {'button button-' + this.props.theme}>{this.props.label}</button>
+			</div>
+		);
+	}
+}
+
+class ThemedDialog extends React.Component {
+	render() {
+		return(
+			<div className = {'dialog dialog-' + this.props.theme}>
+				<h3 className = "dialog-title">{this.props.title}</h3>
+				<div className="dialog-message">{this.props.message}</div>
+				<br/>
+				
+				<ThemedButton theme={this.props.theme} label="Ok"></ThemedButton>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<div>
+					<ThemedBorderBackground theme="red">Hello!</ThemedBorderBackground>
+					<ThemedButton theme="orange" label="Click me!"></ThemedButton>
+					<ThemedDialog theme="orange" title="Dialog title here" message="Dialog message here"></ThemedDialog>
+				</div>, document.getElementById("global-properties-without-context"));
