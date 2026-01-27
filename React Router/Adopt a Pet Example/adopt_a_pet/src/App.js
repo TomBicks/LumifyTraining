52141d00a11ec34aa30b2fec3f9e4da594c29dfc
Thomas Bickley <tombicks@outlook.com>
2026-01-27 14:29:06 +1100

    Added browser router and nested index route

--- React Router/Adopt a Pet Example/adopt_a_pet/src/App.js
+++ React Router/Adopt a Pet Example/adopt_a_pet/src/App.js
@@ -4,13 +4,18 @@ import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
 
-//SELF NOTE!! Last Task completed = 2
+//SELF NOTE!! Last Task completed = 7
 
 // Add react-router-dom imports
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 // create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={ <Root/> }>
+    {/* This is an index route; this one will render into the outlet in <Root> at "/"*/}
+    <Route index element={ <HomePage /> }/>
+  </Route>
+));
 
 function App() {
   return (
