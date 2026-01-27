e853aa05b4be70d70b6b5eba6ecdbc2c3a136a18
Thomas Bickley <tombicks@outlook.com>
2026-01-27 12:02:10 +1100

    ContactPicker works and now validates on submit

--- React Testing Library/appointment-planner-starting-v18/src/components/contactPicker/ContactPicker.js
+++ React Testing Library/appointment-planner-starting-v18/src/components/contactPicker/ContactPicker.js
@@ -9,7 +9,7 @@ export const ContactPicker = ({
   return (
     <div>
       <h4>This is a Contact Picker Element</h4>
-      <select value={value} defaultValue="nothing" name={name} onChange={onChange}>
+      <select required value={value} defaultValue="nothing" name="contact" data-error="Please assign an existing contact to this appointment" onChange={onChange}>
         <option key="nothing" value="">No Contact Selected</option>
         {
           //Returns a list of options for each contact in the contacts list; option selected's value is the contact's name only (so we don't need to send across all their details to simply display who the appointment is for)
